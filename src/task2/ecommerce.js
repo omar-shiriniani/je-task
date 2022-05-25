b6c1600b9e5d13d7f9a373dc770246be0c36ec4d
OmarShiriniani <omar@unipay.com>
2022-05-25 20:46:30 +0400

    task2

--- src/task2/ecommerce.js
+++ src/task2/ecommerce.js
@@ -1,32 +1,101 @@
 ////////////////////////////////////////////// Helper code, do not edit /////////////////////////////////////////
-import { allIds, fetchOrderById } from "../api";
+
+// I changed the import from '../api' to '../api/index.js', because first version of import filed and makes error
+import { allIds, fetchOrderById } from '../api/index.js';
 
 ////////////////////////////////// Your code tasks is below //////////////////////////////////////////////////////
 
-// this file does not work, can't run ecommerce.js
+import moment from 'moment';
 
 const fetchAllOrders = () => {
-    const ids = allIds;
-    // .....
-    //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
+  const ids = allIds;
+
+  for (let index = 0; index < ids.length; index++) {
+    fetchOrderById(ids[index])
+      .then((order) => {
+        // console.log(order);
+        return order;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  //   1. TODO: fetch all ids using the "fetchOrderById" and the given ids, make it work as efficient and clean as possible.
 };
 
 const bucketOrdersByUsers = () => {
-    let ordersByUsers = {};
-    //   2. TODO: using the function from section 1 you should now bucket the orders by user.
-    // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
-    return ordersByUsers;
+  let ordersByUsers = {};
+  const ids = allIds;
+
+  for (let index = 0; index < ids.length; index++) {
+    fetchOrderById(ids[index])
+      .then((group) => {
+        if (typeof ordersByUsers[group.userId] === 'undefined') {
+          ordersByUsers[group.userId] = [];
+        }
+        ordersByUsers[group.userId].push({
+          id: group.id,
+          timestamp: group.timestamp,
+          title: group.title,
+        });
+        console.log(ordersByUsers);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  //   2. TODO: using the function from section 1 you should now bucket the orders by user.
+  // each key in the object (ordersByUsers) represents a userId and each value is an array of the orders of that user.
+  return ordersByUsers;
 };
 
 const getLast2WeeksOrders = () => {
-    //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
+  const ids = allIds;
+  let startDate = moment().subtract(14, 'days').format('YYYY-MM-DD');
+  let endDate = moment().format('YYYY-MM-DD');
+  for (let index = 0; index < ids.length; index++) {
+    fetchOrderById(ids[index])
+      .then((order) => {
+        let date = moment(order.timestamp).format('YYYY-MM-DD');
+        if (date >= startDate && date <= endDate) {
+          console.log(order);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  //   3. TODO: fetch all Ids and return array with only the last 2 weeks orders. make it work as efficient and clean as possible.
 };
 
 const bucketOrdersByDate = () => {
-    let ordersByDate = {};
-    //   4. TODO: using the function from section 3 bucket the orders by date.
-    // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
-    return ordersByDate;
+  let ordersByDate = {};
+  const ids = allIds;
+
+  for (let index = 0; index < ids.length; index++) {
+    fetchOrderById(ids[index])
+      .then((group) => {
+        let date = moment(group.timestamp).format('YYYY-MM-DD');
+        if (typeof ordersByDate[date] === 'undefined') {
+          ordersByDate[date] = [];
+        }
+        ordersByDate[date].push({
+          id: group.id,
+          timestamp: date,
+          title: group.title,
+        });
+        console.log(ordersByDate);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  //   4. TODO: using the function from section 3 bucket the orders by date.
+  // each key in the object (ordersByDate) represents a day and each value is an array of the orders in that date.
+  return ordersByDate;
 };
 
 fetchAllOrders();
